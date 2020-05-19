89b1a7046565828e320fa6aac138cf05b3654bce
Enrique Valencia <enrique.valencia@yale.edu>
2020-05-19 01:20:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [] << "Kawhi"
+  array = ["Jordan", "Kobe", "Lebron", "Magic"]
+  array << "Kaehi"
 end
 
 def add_element_to_start_of_array(array, element)
